cfef5eff7592bc0b96fd6d39cb236d922198d258
junaidAhmed8819 <junaidahmed882000@gmail.com>
2021-12-27 13:15:07 +0530

    Is Palindrome or not

---
+++ src/CheckForPalindrome.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class CheckForPalindrome {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        boolean ans = isPalindrome(str);
+        System.out.println(ans);
+    }
+
+    private static boolean isPalindrome(String str) {
+        char[] charArray = str.toCharArray();
+
+        int start = 0;
+        int end = charArray.length-1;
+        int flag = 0;
+        while(start<end){
+            if(charArray[start]==charArray[end]){
+                flag = 1;
+                start++;
+                end--;
+            }else{
+                break;
+            }
+        }
+        System.out.println(Arrays.toString(charArray));
+        return flag == 1;
+    }
+}
